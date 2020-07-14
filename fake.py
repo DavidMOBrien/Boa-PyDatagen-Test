550867b132feee439e1929e46eaf0da4e8f69c5f
DavidMOBrien <obriedaa@uni.edu>
2020-07-14 11:22:33 -0500

    added fake.py

---
+++ fake.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 11:19:18 2020
+
+@author: deeeo
+"""
+
+import sklearn
+import math as m
+import os,glob as o,g
+
+
+
+def fakeTest1(parameter1, parameter2):
+    return { 'key': 'value1', 'key2': 'value2'}
+
+def fakeTest2():
+    
+    m.sqrt(55)
+    
+    return 'done'
+
+def fakeTest3(aList):
+    
+    print(aList.split(','))
+    
+    return aList[3]
